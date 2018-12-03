8573ce1b6525a363b4d61ee2a1f416dd64e91b80
Sneakyy <thorasuarus@gmail.com>
2018-12-03 09:44:55 -0600

    Update Color.cpp

--- Project1/Color.cpp
+++ Project1/Color.cpp
@@ -1,7 +1,9 @@
 #include "Color.h"
+//Colors used by GUI and ESP
 Color Red = Color(255, 0, 0, 255);
 Color Green = Color(0, 255, 0, 255);
 Color White = Color(255, 255, 255, 255);
 Color Black = Color(10, 10, 10, 190);
 Color T = Color(170, 10, 30, 255);
 Color CT = Color(10, 170, 30, 255);
+Color Spec = Color(0, 0, 0, 255)
